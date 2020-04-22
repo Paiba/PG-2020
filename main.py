f07601caecca54dfea4e05efd070555ab0db3e53
Paiba <pedropaivaladeira@hotmail.com>
2020-04-22 03:49:43 -0300

    ideia da tabela_refinada pronta, faltam alguns ajustes referentes a inconsistências no número de disciplinas e quanto a valores oscilantes nas últimas 6 colunas dos dados passados.

--- main.py
+++ main.py
@@ -18,31 +18,21 @@ root.withdraw()
 file_path = filedialog.askopenfilename()
 
 tabela_bruta = pd.read_csv(file_path)
-
 # Gráficos e Tabelas Úteis
 
-#FUNCAO QUE TRATA OS DADOS
-def alunos(df):
-	"Esta funcao cria um dataframe refinado dos dados brutos. O dataframe refinado não trata cada linha como uma disciplina ligada ao aluno e sim cada linha como um aluno. Algumas colunas não serão preservadas e outras serão adicionadas **********ANOTAR AQUI QUAIS SOMEM E QUAIS SAO CRIADAS*************************"
-	nome_linhas = list()
-	dados = [ ]
-	for i in df.itertuples():
-		id_aluno = i.ID_CURSO_ALUNO;
-		if id_aluno not in nome_linhas :
-			nome_linhas.append(id_aluno)
+#variável com todas as colunas que se repetem no dado bruto (Faltam as últimas 6 que deram problemas de inconsistencias)
+colunas_repetidas = ['ID_CURSO_ALUNO','COD_CURSO','NOME_CURSO','ANO_INGRESSO','FORMA_INGRESSO','FORMA_EVASAO','PERIODO_ALUNO','TIPO_INSTUICAO_SEGUNDO_GRAU','NACIONALIADE','NATURALIDADE','UF_NATURALIDADE','COTISTA','PLANO_ESTUDO']
 
-	
-	print(nome_linhas);
-			
-		
-	
-	dfr = pd.DataFrame(dados, index = nome_linhas);
-	return dfr;
+#coluna com a quantidade de disciplinas que o aluno fez
+disciplinas = tabela_bruta.groupby(colunas_repetidas).COD_DISCIPLINA.count().to_frame()
 
-#FIM DA FUNÇÃO QUE TRATA DADOS
+#coluna com a média não ponderada das notas do aluno
+media_final = tabela_bruta.groupby(colunas_repetidas).MEDIA_FINAL.mean().to_frame()
 
-tabela_refinada = alunos(tabela_bruta)
+#junção das informações
+tabela_refinada = pd.concat([media_final, disciplinas], axis=1, sort=False)
 
 print(tabela_refinada);
+
 
 

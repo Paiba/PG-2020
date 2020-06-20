7146c15e8193dc53f15b6c48110f04e3647cedb2
Paiba <pedropaivaladeira@hotmail.com>
2020-06-20 20:03:43 -0300

    Aba academico modificada: cada gráfico agora possui uma função diferente e um contrutor que já as cria

--- aba_academico.py
+++ aba_academico.py
@@ -15,33 +15,31 @@ from bokeh.transform import factor_cmap
 from bokeh.transform import cumsum
 from math import pi
 ###
-class Aba_academico:
+class Aba_academico: 
 
-         def __init__(self, dados):
-
-                data1 =  dados[dados.FORMA_EVASAO == 'Insucesso Acadêmico'].reset_index()
-                
-                #Quantidade de alunos evadidos absoluta por reprovações por falta
-
-                hist_falt, edges_falt = np.histogram(data1['NUM_REP_FALTA'], density=False, bins=50)
+        #Quantidade de alunos evadidos absoluta por reprovações por falta
+        def grafico1(self): 
+                hist_falt, edges_falt = np.histogram(self.data['NUM_REP_FALTA'], density=False, bins=50)
                 rep_falta = figure( plot_width=500, plot_height=300,title= "Reprovações por FALTA entre Alunos que Evadiram em 2018",toolbar_location=None)
                 rep_falta.quad(top=hist_falt, bottom=0, left=edges_falt[:-1], right=edges_falt[1:], line_color="white")
                 rep_falta.xaxis.axis_label = "Número de Reprovações"
                 rep_falta.yaxis.axis_label = "Quantidade de Alunos"
-                
-                
-                #Quantidade de alunos evadidos absoluta por reprovações por nota
-                hist_nota, edges_nota = np.histogram(data1['NUM_REP_NOTA'], density=False, bins=50)
+                return rep_falta;
+
+        #Quantidade de alunos evadidos absoluta por reprovações por nota
+        def grafico2(self):
+                hist_nota, edges_nota = np.histogram(self.data['NUM_REP_NOTA'], density=False, bins=50)
                 rep_nota = figure( plot_width=500, plot_height=300,title= "Reprovações por NOTA entre Alunos que Evadiram em 2018",toolbar_location=None)
                 rep_nota.quad(top=hist_nota, bottom=0, left=edges_nota[:-1], right=edges_nota[1:], line_color="white")
                 rep_nota.xaxis.axis_label = "Número de Reprovações"
                 rep_nota.yaxis.axis_label = "Quantidade de Alunos"
-
-                #Distribuição de alunos que evadiram por Coeficiente de Rendimento
+                return rep_nota;
+        #Coeficiente de Rendimento dos alunos desistentes
+        def grafico3(self):
                 cr = [0,0,0]
                 x=[1,2,3]
                 faixa = ['<5', '>5 e <7','>7']
-                for i in data1['MEDIA_PONDERADA']:
+                for i in self.data['MEDIA_PONDERADA']:
                         if i<=5:
                                cr[0]=cr[0]+1
                         elif i<=7:
@@ -57,10 +55,12 @@ class Aba_academico:
                 rendimento.xaxis.major_label_overrides = {1: '<5', 2: '>5 e <7', 3: '>7'}
                 rendimento.y_range.start = 0
                 #rendimento.varea_stack(x,cr, color="gray", alpha=0.5, source=source) #PINTAR A AREA EMBAIXO
+                return rendimento;
 
-                #Frequência de presença
+        #Frequência de presença de alunos desistentes
+        def grafico4(self):
                 freq = [0,0,0]
-                for i in data1['PORCENTAGEM_FALTAS']:
+                for i in self.data['PORCENTAGEM_FALTAS']:
                         if i<=10:
                                freq[0]=freq[0]+1
                         elif i<=25:
@@ -76,8 +76,17 @@ class Aba_academico:
                 freq_falta.wedge(x=0, y=1, radius=0.4, start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),line_color="white", fill_color='color', legend='frequencia', source=fonte)
                 freq_falta.x_range.start = -0.6
                 freq_falta.legend.location = "top_right"
+                return freq_falta;
 
 
-                
-
+        #CONSTRUTOR DA ABA
+        def __init__(self, dados):
+                self.data =  dados[dados.FORMA_EVASAO == 'Insucesso Acadêmico'].reset_index()
+                rep_falta = self.grafico1()
+                rep_nota = self.grafico2()
+                rendimento =  self.grafico3()
+                freq_falta = self.grafico4()
                 self.aba = layout([[rep_falta,rep_nota],[rendimento,freq_falta]])
+
+            
+                
